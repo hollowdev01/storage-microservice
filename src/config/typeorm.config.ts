27b11a61fcda8976c210355d532a9e8c4ca88fca
hollowdev01 <dev.work011021@gmail.com>
2024-10-19 02:36:31 -0400

    feat: :sparkles: Add config for typeorm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from './envs.config';
+
+export const getTypeOrmModuleConfig = (): TypeOrmModuleOptions =>
+  ({
+    type: 'postgres',
+    host: config.DB_HOST,
+    port: config.DB_PORT,
+    database: config.DB_NAME,
+    username: config.DB_USER,
+    password: config.DB_PASSWORD,
+    entities: [__dirname + './../**/*.entity{.ts,.js}'],
+    synchronize: true,
+    migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  }) as TypeOrmModuleOptions;
+
+export default new DataSource(getTypeOrmModuleConfig() as DataSourceOptions);
