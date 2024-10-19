2cefe5fabdf74beff1032cfe3524f831561b396e
hollowdev01 <dev.work011021@gmail.com>
2024-10-19 11:01:17 -0400

    feat: :memo: Add doc

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { config } from './config/envs.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useLogger(app.get(Logger));
+
+  const doc = new DocumentBuilder()
+    .setTitle('Images API')
+    .setDescription('API for managing images')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, doc);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(config.PORT);
   console.log(`Application is running on port ${config.PORT}`);
--- src/modules/images/images.controller.ts
+++ src/modules/images/images.controller.ts
@@ -15,13 +15,17 @@ import { imageFilter } from '../../utils/image-filters';
 import { FilterImages } from './dtos/filter-images.dto';
 import { GetUser } from '../../shared/decorators/get-user.decorator';
 //import { AuthGuard } from '@nestjs/passport';
+import { ApiUploadBullImages } from '../../shared/decorators/swagger-file.decorator';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('images')
+@ApiTags('Images')
 //@UseGuards(AuthGuard('jwt'))
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
   @Post()
+  @ApiUploadBullImages()
   @UseInterceptors(
     FilesInterceptor('files', 3, {
       fileFilter: imageFilter,
@@ -38,16 +42,45 @@ export class ImagesController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Retrieve all images',
+    description: 'Get a list of all images with optional filters.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved list of images.',
+  })
   async findAllImages(@Query() filters: FilterImages) {
     return await this.imagesService.findAllImages(filters);
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Retrieve image by ID',
+    description: 'Get the details of a specific image by its ID.',
+  })
+  @ApiParam({ name: 'id', description: 'The UUID of the image.', type: String })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved image details.',
+  })
+  @ApiResponse({ status: 404, description: 'Image not found.' })
   async findById(@Param('id', ParseUUIDPipe) id: string) {
     return await this.imagesService.findById(id);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete image',
+    description: 'Delete an image by its ID.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The UUID of the image to delete.',
+    type: String,
+  })
+  @ApiResponse({ status: 200, description: 'Image successfully deleted.' })
+  @ApiResponse({ status: 404, description: 'Image not found.' })
   async deletedImage(@Param('id', ParseUUIDPipe) id: string) {
     return await this.imagesService.deletedImage(id);
   }
